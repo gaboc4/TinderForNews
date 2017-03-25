056182103d6ea98e1cd1d34bc3838051c19754fc
Gabriel <gabrielc007@gmail.com>
2017-03-24 21:00:17 -0400

    First Commit

--- Tinder for News/ViewController.swift
+++ Tinder for News/ViewController.swift
@@ -2,8 +2,8 @@
 //  ViewController.swift
 //  Tinder for News
 //
-//  Created by Gabriel Centeno on 3/24/17.
-//  Copyright © 2017 Gabriel Centeno. All rights reserved.
+//  Created by Gabriel Centeno and Natalie Alexander on 3/24/17.
+//  Copyright © 2017 Gabriel Centeno, Natalie Alexander . All rights reserved.
 //
 
 import UIKit
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("First push to GitHub")
     }
 
     override func didReceiveMemoryWarning() {

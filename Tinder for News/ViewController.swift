448d6819832fdc0c6b9c055a91e9d3db5e589209
Gabriel <gabrielc007@gmail.com>
2017-03-24 20:57:28 -0400

    Initial Commit

---
+++ Tinder for News/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tinder for News
+//
+//  Created by Gabriel Centeno on 3/24/17.
+//  Copyright © 2017 Gabriel Centeno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
